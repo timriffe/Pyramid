e66f2ee99e496926605dfb2159f53c4d1234de47
triffe <tim.riffe@gmail.com>
2013-06-06 16:49:16 +0200

    fast roxygenization

--- Pyramid/R/MyersI.R
+++ Pyramid/R/MyersI.R
@@ -1,3 +1,36 @@
+#'
+#'Calculate Myers' blended index for age heaping based on digit preference.
+#'
+#'Calculate Myers' blended index for age heaping based on digit preference.
+#'Implemented as described in Shyrock et. al. (1980).
+#'
+#'Large numbers mean more bias in digit preference. Numbers less than 1 can be
+#'disregarded. Interpret as percent of population that would need to be moved
+#'to a different age category for there to be no noticeable pattern of
+#'preference in age declaration.
+#'
+#'@param x vector of age-specific population counts
+#'@param ages vector of completed ages.
+#'@return the blended index value is returned.
+#'@note This implementation will only work for digits 0-9: not months, zodiacs
+#'or anything else that doesn't have a period of 10
+#'@author Tim Riffe
+#'@references Myers, R.J. (1940) Errors and bias in the reporting of ages in
+#'census data. Transactions of the Actuarial Society of America. vol 41 nr 2
+#'pages 395--415\cr Shryock, H.S. and Siegel, J.S. and Larmon, E.A. (1980) The
+#'methods and materials of demography. US Dept. of Commerce, Bureau of the
+#'Census (pages 116-118)
+#'@examples
+#'
+#'data(PTpop)
+#'
+#'# separately for males and females in both years
+#'apply(PTpop,2,MyersI)
+#'
+#'# males and females together, 1950
+#'MyersI(rowSums(PTpop[,1:2]))
+#'
+
 MyersI <-
 function(x,ages){
 	if (missing(ages)) { 
