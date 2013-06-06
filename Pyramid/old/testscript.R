43511323ea3c98942d1b4f642eac55f7de2009b4
triffe <tim.riffe@gmail.com>
2013-06-06 15:44:28 +0200

    roxygenized. added script for test building. should implement automatic versioning

---
+++ Pyramid/old/testscript.R
@@ -0,0 +1,8 @@
+
+# Author: triffe
+###############################################################################
+
+# this script is for test package building a light testing
+
+devtools::build("/home/triffe/git/Pyramid/Pyramid","/home/triffe/git/Pyramid/Pyramid")
+
